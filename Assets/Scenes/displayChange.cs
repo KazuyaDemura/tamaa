ee7c717bbabddcc8ecee3139b2ba03516eb7e9bd
Kazuya <47873854+KazuyaDemura@users.noreply.github.com>
2019-02-22 15:01:51 +0900

    add Scenes

---
+++ Assets/Scenes/displayChange.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class displayChange : MonoBehaviour
+{
+
+    void Start()
+    {
+
+    }
+
+    //オブジェクトが衝突したとき
+    void OnCollisionEnter(Collision other)
+    {
+        if (other.gameObject.tag == "DMURA")
+        {
+            SceneManager.LoadScene("GAMEOVER");
+        }
+
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "DMURA")
+        {
+            SceneManager.LoadScene("GAMEOVER");
+        }
+    }   
+}
